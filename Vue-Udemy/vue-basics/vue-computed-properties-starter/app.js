898adce3f549772e37d31510533416d0cd3cf22b
Ventsislav Peychev <del.busque@gmail.com>
2022-08-27 06:59:24 +0200

    Added computed properties

---
+++ Vue-Udemy/vue-basics/vue-computed-properties-starter/app.js
@@ -0,0 +1,21 @@
+let taxRate = 1.23;
+
+const vm = Vue.createApp({
+  data() {
+    return {
+      price: 4,
+      items: 0
+    }
+  },
+  computed: {
+    taxRate() {
+      return (taxRate * 100) - 100;
+    },
+    includingVat() {
+      return (this.price * taxRate).toFixed(2);
+    },
+    total() {
+      return (this.includingVat * this.items).toFixed(2);
+    }
+  }
+}).mount('#app');
