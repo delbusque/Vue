938c7e0c3d0ae6ec52c04751eb2b8ae3058290b4
Ventsislav Peychev <del.busque@gmail.com>
2022-09-07 05:30:31 +0200

    Added component props

--- Vue-Udemy/components-basics/components-starter/app.js
+++ Vue-Udemy/components-basics/components-starter/app.js
@@ -6,8 +6,37 @@ const app = Vue.createApp({
   computed: {}
 });
 
-app.component('my-first-component', {
-  template: `<div>Hello World !!!</div>`
+app.component('todo-list-item', {
+  props: {
+    task: {
+      type: String,
+      required: true,
+      validator(value) {
+        return 'okey' === value;
+      }
+    },
+    id: {
+      type: Number,
+      required: false,
+      validator(value) {
+        return value >= 5 && value <= 10;
+      }
+    },
+    tickets: {
+      type: Array,
+      required: false,
+      validator(value) {
+        return value.length == 2;
+      }
+    },
+    onboard: {
+      type: Boolean,
+      validator(value) {
+        return value === true;
+      }
+    }
+  },
+  template: `<div class="bg-white shadow-sm rounded-md text-gray-700 text-xs md:text-sm p-4">{{task}}</div>`
 })
 
 app.mount('#app');
